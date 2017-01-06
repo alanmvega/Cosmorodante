a406a9e588e57e3cedd70c1f9ce97fce849702f1
alanvega <alvega@despegar.com>
2017-01-06 14:45:34 -0300

    HelloApplication

--- src/Main/java/app/HelloApplication.java
+++ src/Main/java/app/HelloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(HelloApplication.class, args);
     }
 
 }
