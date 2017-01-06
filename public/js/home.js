c96350708b9aefd60817eb558db9f8e9a49e5c2d
Alan Vega <alanmvega@gmail.com>
2017-01-05 21:07:47 -0300

    navs

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,16 @@
-var app = angular.module('project');
+var myApp = angular.module('myApp', []);
 
-app.controller("HomeController", function ($scope, $http) {
+myApp.controller("HomeController", function ($scope, $http) {
+    var homeCtrl = this;
 
+    homeCtrl.navLinks =[
+        {"text": "¿DÓNDE ESTAMOS?", "ref": "/donde-estamos"},
+        {"text": "¿QUIÉNES SOMOS?", "ref": "/quienes-somos"},
+        {"text": "SEMINARIOS", "ref": "/seminarios"},
+        {"text": "TALLERES", "ref": "/talleres"},
+        {"text": "TERAPIAS", "ref": "/terapias"},
+        {"text": "ESPECTÁCULOS", "ref": "/espectaculos"},
+        {"text": "FOTOS", "ref": "/fotos"},
+        {"text": "CONTACTO", "ref": "/contacto"}];
 });
+
